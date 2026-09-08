c7b939c57a269e42358852c8cdc27a4bc15ecaf3
김민서 <minseokim0113@gmail.com>
2026-09-08 15:02:57 +0900

    feat(BNC-14): establish test strategy

---
+++ src/test/java/com/eum/banca/support/MySqlTestcontainersConfiguration.java
@@ -0,0 +1,21 @@
+package com.eum.banca.support;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class MySqlTestcontainersConfiguration {
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>(DockerImageName.parse("mysql:8.4"))
+			.withDatabaseName("bancassurance")
+			.withUsername("banca")
+			.withPassword("banca")
+			.withEnv("TZ", "UTC");
+	}
+}
